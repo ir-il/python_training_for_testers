0fb6b3c35eb33fc22dc83f1caa40318d01471aef
Iryna Ilina <ny-blin@ya.ru>
2015-03-18 00:21:00 +0200

    Добавлен класс contact

---
+++ contact.py
@@ -0,0 +1,13 @@
+__author__ = 'Iryna Ilina'
+
+class Contact:
+
+    def __init__(self, firstname, lastname, position, company, mobile, day, year, notes):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.position = position
+        self.company = company
+        self.mobile = mobile
+        self.day = day
+        self.year = year
+        self.notes = notes
