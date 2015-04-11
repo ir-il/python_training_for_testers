6e608ab9c2a207dff16acb3e26650a0bfd37c455
Iryna Ilina <ny-blin@ya.ru>
2015-04-11 23:00:27 +0300

    созданы пакеты для разных зон ответственности

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 import pytest
-from contact import Contact
-from application import Application
+from model.contact import Contact
+from fixture.application import Application
 
 @pytest.fixture
 def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
+
 
 def test_test_add_contact(app):
     app.login(username="admin", password="secret")
