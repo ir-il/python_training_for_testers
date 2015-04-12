03fc8f8ff257de30730547de3bc5cb3bc00c33c9
Iryna Ilina <ny-blin@ya.ru>
2015-04-12 18:04:49 +0300

    Создан вспом. метод для заполнения формы группы
    Создан метод fill_group_form, чтобы не повторять одинаковые куски кода в
    создании и редактировании групп.

--- fixture/group.py
+++ fixture/group.py
@@ -17,6 +17,13 @@ class GroupHelper():
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
+        self.fill_group_form(group)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def fill_group_form(self, group):
+        wd = self.app.wd
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -26,29 +33,17 @@ class GroupHelper():
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
-
 
     def edit_first_group(self, group):
         wd = self.app.wd
         self.open_groups_page()
         # select first group
         wd.find_element_by_name("selected[]").click()
-        # submit editing
+        # start editing
         wd.find_element_by_name("edit").click()
         # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group editing
+        self.fill_group_form(group)
+        # save changes
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
