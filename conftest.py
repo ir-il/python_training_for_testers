f64131f9add1f567a4f56ff7d78e4bc28447e1a0
Iryna Ilina <nyblinnn@gmail.com>
2015-05-09 14:39:13 +0400

    Задание16: Реализованы опции командной строки,
    ...позволяющие указывать тип браузера и адрес тестируемого приложения

--- conftest.py
+++ conftest.py
@@ -7,14 +7,17 @@ from fixture.application import Application
 
 fixture = None
 
+
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
+            fixture = Application(browser=browser, base_url=base_url)
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
@@ -26,3 +29,8 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
--- fixture/application.py
+++ fixture/application.py
@@ -1,20 +1,30 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Irsen'
 
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
 
+
+
 class Application:
 
-    def __init__(self):
-        self.wd = WebDriver()
-        #дополнительное ожидание необходимо в случае динамического обновления страницы
-        #self.wd.implicitly_wait(5)
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        # дополнительное ожидание необходимо в случае динамического обновления страницы
+        # self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -25,9 +35,10 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_id("maintable")) > 0):
-            wd.get("http://localhost/addressbook/")
-
+# после Задание №16: (опции командной строки, позволяющие указывать тип браузера и адрес)
+# эта проверка становится лишней
+# if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_id("maintable")) > 0):
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
