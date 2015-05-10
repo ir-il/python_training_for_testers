a67b86fdf72755bd489498eb362146fa2b90f8f7
Iryna Ilina <nyblinnn@gmail.com>
2015-05-10 21:24:45 +0400

    Загрузка тестовых данных из файла .py
    Реализовано только для добавления групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,21 +3,8 @@ __author__ = 'Iryna Ilina'
 
 from model.group import Group
 import pytest
-import random
-import string
+from data.add_group import constant as testdata
 
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-testdata = [Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 7), footer=random_string("footer", 5))
-    for i in range(5)
-#   for name in ["", random_string("name", 10)]
-#   for header in ["", random_string("header", 7)]
-#   for footer in ["", random_string("footer", 5)]
-]
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group):
