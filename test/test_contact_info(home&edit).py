6675c86b7a88cd9c5557e721421cdde2232c5399
Iryna Ilina <nyblinnn@gmail.com>
2015-05-13 02:08:21 +0400

    Задание 21: Сравнение всех контактов DB с Home page
    Добавлен тест test_home_contact_info_vs_db
    Но он практически копия того, что у меня реализовано в check_ui для
    контактов.

--- test/test_contact_info(home&edit).py
+++ test/test_contact_info(home&edit).py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Iryna Ilina'
 
+from model.contact import Contact
 
 def test_contact_info(app):
     contact_from_home_page = app.contact.get_contact_list()[1]
@@ -10,5 +11,11 @@ def test_contact_info(app):
     assert contact_from_home_page.address == contact_from_edit_page.address
     assert contact_from_home_page.all_phones_from_home_page == app.contact.merge_phones_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.all_emails_from_home_page == app.contact.merge_emails_like_on_home_page(contact_from_edit_page)
+
+
+def test_home_contact_info_vs_db(app, db):
+    home_contacts = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    db_contacts = sorted(map(app.contact.contact_like_on_home_page, db.get_contact_list()), key=Contact.id_or_max)
+    assert home_contacts == db_contacts
 
 
